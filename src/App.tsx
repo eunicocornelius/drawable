6af6ee5c90521f044ed0e6dcc0cefa4ce1ef991f
Nico <eunico.cornelius@gmail.com>
2022-12-22 03:07:01 +0700

    added features = 1.Shapes, 2.Color, 3.Remove, 4.Text, 5.Image(Incomplete), 6.Drawing Mode and tweaked draw button on active style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,146 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import MainCanvas from "./components/MainCanvas";
 import Toolbelt from "./components/Toolbelt";
+import { fabric } from "fabric";
+import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
+import testImage from "./test.jpg";
 
 function App() {
   const [mode, setMode] = useState("");
+  const [drawing, setDrawing] = useState(false);
+
+  const { selectedObjects, editor, onReady } = useFabricJSEditor();
 
   var changeMode = (newMode: string): void => {
     setMode(newMode);
+  };
+
+  var changeDrawing = (): void => {
+    setDrawing(!drawing);
+  };
+
+  useEffect(() => {
+    if (editor) {
+      editor.canvas.backgroundColor = "white";
+    }
+    editor?.canvas?.renderAll();
+    // editor.setFillColor("transparent");
+  }, [editor]);
+
+  useEffect(() => {
+    let imageUpload = document.getElementById("image-file");
+
+    imageUpload?.addEventListener("change", addImage);
+  }, []);
+
+  useEffect(() => {
+    if (editor != undefined) {
+      if (drawing) {
+        editor!.canvas!.isDrawingMode = true;
+      }
+      if (!drawing) {
+        editor!.canvas!.isDrawingMode = false;
+      }
+    }
+  }, [drawing]);
+
+  // useEffect(() => {
+  //   fabric.Image.fromURL("./test.jpg", function (img) {
+  //     editor?.canvas.add(img);
+  //   });
+  // }, [fabric, editor]);
+
+  const onAddCircle = () => {
+    editor?.addCircle();
+  };
+
+  const onAddRectangle = () => {
+    editor?.addRectangle();
+  };
+
+  const onAddImage = () => {
+    document.getElementById("image-file")?.click();
+  };
+
+  const addImage = (e: any) => {
+    let file = e.target.files[0];
+    let reader = new FileReader();
+    reader.onload = function (f: any) {
+      let data = f.target.result;
+      fabric.Image.fromURL(data, function (img) {
+        let oImg = img.set({ left: 0, top: 0, angle: 0 }).scale(0.2);
+        editor?.canvas.add(oImg).renderAll();
+      });
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const onAddTriangle = () => {
+    var triangle = new fabric.Triangle({
+      width: 50,
+      height: 50,
+      fill: "",
+      stroke: "black",
+      strokeWidth: 1,
+      cornerColor: "blue",
+      angle: 0,
+    });
+
+    editor?.canvas.add(triangle);
+  };
+
+  const onAddLine = () => {
+    editor?.addLine();
+  };
+
+  const onAddText = () => {
+    var textEditable = new fabric.Textbox("Editable Textbox", {
+      width: 500,
+      editable: true,
+      fill: "#000000",
+    });
+
+    editor?.canvas.add(textEditable);
+    editor?.canvas?.renderAll();
+  };
+
+  const onRemove = () => {
+    editor?.deleteSelected();
+  };
+
+  const onColorRed = () => {
+    editor?.canvas?.getActiveObjects()?.forEach((element) => {
+      if (element.type === "textbox") {
+        element.set("fill", "#FF0000");
+      } else {
+        element.set("stroke", "#FF0000");
+      }
+    });
+
+    editor?.canvas?.renderAll();
+  };
+
+  const onColorGreen = () => {
+    editor?.canvas?.getActiveObjects()?.forEach((element) => {
+      if (element.type === "textbox") {
+        element.set("fill", "#00FF00");
+      } else {
+        element.set("stroke", "#00FF00");
+      }
+    });
+    editor?.canvas?.renderAll();
+  };
+
+  const onColorBlue = () => {
+    editor?.canvas?.getActiveObjects()?.forEach((element) => {
+      if (element.type === "textbox") {
+        element.set("fill", "#0000FF");
+      } else {
+        element.set("stroke", "#0000FF");
+      }
+    });
+    editor?.canvas?.renderAll();
   };
 
   return (
@@ -21,10 +154,34 @@ function App() {
             <h1 className="text-2xl font-sans pl-10">Drawable</h1>
           </nav>
           <div id="side-toolbelt" className="col-span-2 row-span-19 m-auto">
-            <Toolbelt changeMode={changeMode} />
+            <Toolbelt
+              changeMode={changeMode}
+              changeDrawing={changeDrawing}
+              drawing={drawing}
+              onAddCircle={onAddCircle}
+              onAddRectangle={onAddRectangle}
+              onAddTriangle={onAddTriangle}
+              onAddImage={onAddImage}
+              onAddLine={onAddLine}
+              onAddText={onAddText}
+              onRemove={onRemove}
+              onColorRed={onColorRed}
+              onColorGreen={onColorGreen}
+              onColorBlue={onColorBlue}
+            />
           </div>
           <div id="main-canvas" className="col-span-23 row-span-19 p-20">
-            <MainCanvas />
+            <input
+              type="file"
+              id="image-file"
+              accept="image/jpeg, image/png, image/jpg"
+              className="hidden"
+            />
+            {/* <canvas className="w-full h-full bg-slate-100 shadow-2xl rounded-xl"></canvas> */}
+            <FabricJSCanvas
+              className="sample-canvas w-full h-full shadow-2xl "
+              onReady={onReady}
+            />
           </div>
         </div>
       </div>
--- src/components/Toolbelt.tsx
+++ src/components/Toolbelt.tsx
@@ -1,78 +1,114 @@
 import React from "react";
 
 interface ToolbeltProps {
+  drawing: boolean;
   changeMode: (newMode: string) => void;
+  changeDrawing: () => void;
+  onAddCircle: () => void;
+  onAddRectangle: () => void;
+  onAddTriangle: () => void;
+  onAddImage: () => void;
+  onAddLine: () => void;
+  onAddText: () => void;
+  onRemove: () => void;
+  onColorRed: () => void;
+  onColorGreen: () => void;
+  onColorBlue: () => void;
 }
 
-function Toolbelt({ changeMode }: ToolbeltProps) {
+function Toolbelt({
+  drawing,
+  changeMode,
+  changeDrawing,
+  onAddCircle,
+  onAddRectangle,
+  onAddTriangle,
+  onAddImage,
+  onAddLine,
+  onAddText,
+  onRemove,
+  onColorRed,
+  onColorGreen,
+  onColorBlue,
+}: ToolbeltProps) {
   return (
     <div className="  bg-slate-600 w-14 text-gray-400 p-4 flex space-y-2 rounded-xl flex-wrap">
       <button
         className="hover:text-gray-300 hover:scale-125 transition ease-in-out duration-300"
         value="upload-image"
-        onClick={() => changeMode("upload-image")}
+        onClick={onAddImage}
       >
         <div className="material-icons-round ">drive_folder_upload</div>
       </button>
       <button
         className="hover:text-gray-300 hover:scale-125 transition ease-in-out duration-300"
-        onClick={() => changeMode("square")}
+        onClick={onAddRectangle}
       >
         <div className="material-icons-round ">crop_square</div>
       </button>
-      <button
+      {/* <button
         className="hover:text-gray-300 hover:scale-125 transition ease-in-out duration-300"
-        onClick={() => changeMode("rectangle")}
+        onClick={onAddRectangle}
       >
         <div className="material-icons-outlined">rectangle</div>
-      </button>
+      </button> */}
       <button
         className="hover:text-gray-300 hover:scale-125 transition ease-in-out duration-300"
-        onClick={() => changeMode("triangle")}
+        onClick={onAddTriangle}
       >
         <div className="material-icons-round ">signal_cellular_null</div>
       </button>
       <button
         className="hover:text-gray-300 hover:scale-125 transition ease-in-out duration-300"
-        onClick={() => changeMode("circle")}
+        onClick={onAddCircle}
       >
         <div className="material-icons-round ">panorama_fish_eye</div>
       </button>
       <button
         className="hover:text-gray-300 hover:scale-125 transition ease-in-out duration-300"
-        onClick={() => changeMode("line")}
+        onClick={onAddLine}
       >
         <div className="material-icons ">timeline</div>
       </button>
       <button
         className="text-red-400 hover:text-red-500 hover:scale-125 transition ease-in-out duration-300"
-        onClick={() => changeMode("color-red")}
+        onClick={onColorRed}
       >
         <div className="material-icons-round ">square</div>
       </button>
       <button
         className="text-green-400 hover:text-green-500 hover:scale-125 transition ease-in-out duration-300"
-        onClick={() => changeMode("color-green")}
+        onClick={onColorGreen}
       >
         <div className="material-icons-round ">square</div>
       </button>
       <button
         className="text-blue-400 hover:text-blue-500 hover:scale-125 transition ease-in-out duration-300"
-        onClick={() => changeMode("color-blue")}
+        onClick={onColorBlue}
       >
         <div className="material-icons-round ">square</div>
       </button>
       <button
-        className="hover:text-gray-300 hover:scale-125 transition ease-in-out duration-300"
-        onClick={() => changeMode("pencil")}
+        className={
+          drawing
+            ? "text-white scale-125 transition ease-in-out duration-300"
+            : "hover:text-gray-300 hover:scale-125 transition ease-in-out duration-300"
+        }
+        onClick={changeDrawing}
       >
         <div className="material-icons-outlined">draw</div>
       </button>
       <button
         className="hover:text-gray-300 hover:scale-125 transition ease-in-out duration-300"
-        onClick={() => changeMode("text-field")}
+        onClick={onAddText}
       >
         <div className="material-icons">text_fields</div>
+      </button>
+      <button
+        className="hover:text-gray-300 hover:scale-125 transition ease-in-out duration-300"
+        onClick={onRemove}
+      >
+        <div className="material-icons-outlined">remove_circle</div>
       </button>
     </div>
   );
